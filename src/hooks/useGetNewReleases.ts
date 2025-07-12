3918e6abd9d2d433a08e618c9a29597bd8f79092
Hyun-june <leehyunjune11@naver.com>
2025-07-12 19:12:06 +0900

    [feat] type

--- src/hooks/useGetNewReleases.ts
+++ src/hooks/useGetNewReleases.ts
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { getNewReleases } from "../apis/albumApi";
+import useClientCredentialToken from "./useClientCredentialToken";
 
 const useGetNewReleases = () => {
+  const clientCredentialToken = useClientCredentialToken();
   return useQuery({
     queryKey: ["new-releases"],
     queryFn: () => {
-      return getNewReleases();
+      if (!clientCredentialToken) {
+        throw new Error("No token available");
+      }
+      return getNewReleases(clientCredentialToken);
     },
   });
 };
